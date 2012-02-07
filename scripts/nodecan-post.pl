c3b8847edb2f118bfdf845ac13fa58031fb3b0f6
Kazufumi Ohtani <kzmiff@gmail.com>
2012-02-08 00:24:50 +0900

    add japanese charset support

--- scripts/nodecan-post.pl
+++ scripts/nodecan-post.pl
@@ -4,16 +4,22 @@ use strict;
 use warnings;
 use Furl;
 use File::Basename qw/fileparse/;
+use Encode;
+use Encode::Guess qw/euc-jp shiftjis 7bit-jis/;
 
 use constant NODECAN_URL => 'http://127.0.0.1:3003';
 
 my $extensions = {
-  pl => 'perl',
-  pm => 'perl',
-  js => 'javascript',
-  html => 'html',
-  tmpl => 'html',
-  txt => 'text',
+  txt   => 'text',
+  pl    => 'perl',
+  pm    => 'perl',
+  js    => 'javascript',
+  html  => 'html',
+  tmpl  => 'html',
+  tpl   => 'html',
+  xml   => 'xml',
+  m     => 'oc',
+  java  => 'java',
 };
 
 die 'missing arguments' if scalar(@ARGV) == 0;
@@ -23,39 +29,75 @@ my $filepath = $ARGV[0];
 die "file doesn't exist" unless -f $filepath;
 
 # read file
-open(my $fh, '<', $filepath) or die;
-my $body = '';
-while (<$fh>){
-  $body .= $_;
-}
-close($fh);
+my $file_content = read_file($filepath);
+
+# resolve file encoding
+my $body = resolve_encoding($file_content);
 
 # check extension
-my ($filename, $directories, $suffix) = fileparse($filepath, qr/\.[^.]*$/);
-die "file name doesn't match" unless length($suffix) > 0;
-
-my $syntax = 'text';
-my $extension = substr($suffix, 1);
-if (exists($extensions->{$extension})) {
-  $syntax = $extensions->{$extension};
-}
+my $syntax = get_syntax($filepath);
 
 # post file
-my $furl = Furl->new(agent => 'Madoka-Magica/chu-2');
-my $res = $furl->request(
+my $bin_url = post_text($body, $syntax);
+
+print $bin_url . "\n";
+
+exit;
+
+# read file
+sub read_file {
+  my $filepath = shift;
+
+  open(my $fh, '<', $filepath) or die;
+  my $file_content = '';
+  while (<$fh>){
+    $file_content .= $_;
+  }
+  close($fh);
+
+  return $file_content;
+}
+
+# resolve file encoding
+sub resolve_encoding {
+  my $file_content = shift;
+
+  my $encode = guess_encoding($file_content);
+  ref($encode) or die "Can't guess: $encode";
+  my $body = $encode->decode($file_content);
+
+  return Encode::encode('utf8', $body);
+}
+
+sub get_syntax {
+  my $filepath = shift;
+
+  my ($filename, $directories, $suffix) = fileparse($filepath, qr/\.[^.]*$/);
+  die "file name doesn't match" unless length($suffix) > 0;
+
+  my $syntax = 'text';
+  my $extension = substr($suffix, 1);
+  if (exists($extensions->{$extension})) {
+    $syntax = $extensions->{$extension};
+  }
+
+  return $syntax;
+}
+
+sub post_text {
+  my $furl = Furl->new(agent => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.75 Safari/535.7');
+  my $res = $furl->request(
     method  => 'POST',
     url     => NODECAN_URL . '/api/add',
     content => {
       body   => $body,
       syntax => $syntax,
     },
-);
+  );
 
-die "request failed" unless $res->code == 200;
-my $content = $res->content;
+  die "request failed" unless $res->code == 200;
+  my $content = $res->content;
 
-my $bin_url = NODECAN_URL . $content;
-print $bin_url . "\n";
-
-exit;
+  return NODECAN_URL . $content;
+}
 
